53bf1cdbaa68c401408e17ae46d748f9baef3408
apexvalue <43234332+apexvalue@users.noreply.github.com>
2019-06-03 18:15:06 +0800

    updated the messages

--- assets/bots/home/home-bot.js
+++ assets/bots/home/home-bot.js
@@ -1,176 +1,280 @@
 
 var botui = BotUI('home-index');
+var guestname = 'Guest';
+var feelingmode = 'Nice';
 
-botui.message.add({
-  content: 'Hello! 👋'
-}).then(function () {
-  return botui.message.add({
-    //loading: true, // pretend like we are doing something
-    delay: 1500,
-    content: 'Looking for help? How can we help you today?'
-  });
-}).then(function () {
-  return botui.message.add({
-    loading: true, // pretend like we are doing something
-    delay: 500,
-    content: 'Just pick an option:'
-  });
-}).then(function () {
-  return botui.action.button({
-    delay: 500,
-    action: [{
-      text: 'Get Started',
-      value: 'getstarted'
-    }, {
-      text: 'Help me!',
-      value: 'helpme'
-    }]
-  });
-}).then(function () {
-  return botui.message.add({
-    delay: 2500,
-    content: 'Is a new way to share your offers 🕶'
-  });
-}).then(function () {
-  return botui.message.add({
-    delay: 2500,
-    content: 'More engaging and interactive.'
-  });
-}).then(function () {
-  return botui.message.add({
-    delay: 2500,
-    content: 'Want to know more about the history of conversational 🤖 bots? Or do you want to find out now how to create your own conversational landing page?'
-  });
-}).then(function () {
-  return botui.action.button({
-    delay: 3500,
-    action: [{
-      text: 'History!',
-      value: 'history'
-    }, {
-      text: 'Landing pages',
-      value: 'landing'
-    }]
-  });
-}).then(function (res) {
-  //ga_record('btn_click', res.value); google analitics lock execution
-  if(res.value == 'history') {
-    story();
-  }
-  if(res.value == 'landing') {
-    land();
-  }
-});
+botui.message.bot('Hello! 👋 Welcome!')
+  .then(function () {
+    return botui.message.add({
+      loading: true,
+      delay: 500,
+      content: 'I\'m Chatbot, a bot by Apexvalue!'
+    });
+  })
+  .then(function () {
+    return botui.message.add({
+      loading: true,
+      delay: 500,
+      content: 'I\'m feeling good today. How about you ?'
+    });
+  })
+  .then( function() {
+      return botui.action.button({ // 
+        loading: true,
+        delay: 500,
+        action: [{
+            text: 'Good',
+            value: 'good'
+          },{
+            text: 'Really Good',
+            value: 'really_good'
+          },{
+            text: 'Really, really Good',
+            value: 'awfully_good'
+        }]
+      });
+  })
+  .then( function(res) {
+    ga_record('btn_click', res.value); //google analitics lock execution
+    feelingmode = res.text.toLowerCase();
+      return botui.message.bot({
+        loading: true, // pretend like we are doing something
+        delay: 500,
+        content: 'You are feeling ' + feelingmode + '!'
 
-var story = function () {
+      });
+  })
+  .then( function() {
+    botui.message
+    .bot({
+          delay: 1200,
+        loading: true,
+          content: 'By the way, what\'s your name?'
+      })
+  })
+  .then(function () {
+          return botui.action.text({
+            delay: 500,
+            action: {
+                //size: 38,
+                //icon: 'user-circle-o',
+                placeholder: 'Your Name'
+            }
+          });
+      })
+      .then(function(res) {
+          ga_record('btn_click', res.value); //google analitics lock execution
+          guestname = res.value; // save new value
+          botui.message
+          .bot({
+              delay: 1000,
+              //loading: true,
+              content: 'Awesome! Nice to meet you <b>' + guestname + '</b>! ![hello image](/assets/img/hello.gif)'
+          });
+  })
+  .then(function () {
+    return botui.message.add({
+      delay: 1500,
+      content: 'Looking for help? How can we help you today?'
+    });
+  })
+  .then(function () {
+    return botui.message.add({
+      delay: 500,
+      content: 'Just pick an option:'
+    });
+  })
+  .then(function () {
+    return botui.action.button({
+      delay: 500,
+      action: [{
+        text: 'Products',
+        value: 'products1'
+      }, {
+        text: 'No, Give me other options',
+        value: 'moreoptions1'
+      }]
+    });
+  })
+  .then(function (res) {
+    ga_record('btn_click', res.value); //google analitics lock execution
+    if(res.value == 'products') {
+      products();
+    }
+    if(res.value == 'moreoptions1') {
+      moreoptions();
+    }
+  });
+
+var products = function () {
   botui.message.add({
-    delay: 2000,
-    content: "Awesome! Let me explain how a conversational user experience can help your projects:"
-  }).then(function () {
-    return botui.message.add({
-      delay: 3000,
-      content: '1) may be a strategy for involving your users, thanks to the method of using the content you intend to divulge.'
+    delay: 1000,
+    content: "Awesome! Please select the products you want :"
+  })
+  .then(function () {
+    return botui.action.button({
+      delay: 500,
+      action: [{
+        text: 'Automotive',
+        value: 'automotive1'
+      }, {
+        text: 'Weather',
+        value: 'weather1'
+      }, {
+        text: 'GPS',
+        value: 'gps1'
+      }, {
+        text: 'Miscellaneous',
+        value: 'miscellaneous1'
+      }]
     });
-  }).then(function () {
-    return botui.message.add({
-      delay: 3000,
-      content: '2) You can use it to collect lead and user information more fluidly, increasing engagement opportunities.'
-    });
-  }).then(function () {
-    return botui.message.add({
-      delay: 3000,
-      content: '3) Bring your conversation to a more confidential level.'
-    });
-  }).then(function () {
-    return botui.message.add({
-      delay: 2200,
-      content: 'Can I know your name?'
-    });
-  }).then(function () {
-    return botui.action.text({
-      delay: 800,
-      action: {
-        value: 'John Doe',
-        placeholder: 'Your name'
-      }
-    });
-  }).then(function (res) {
-    return botui.message.bot({
-      delay: 1500,
-      content: res.value + ' nice to meet you!'
-    });
-  }).then(function (res) {
-    return botui.message.bot({
-      delay: 1500,
-      content: 'Such a layout allows you to share only information that may interest readers and skip unnecessary ones to those who are not interested, by means of targeted questions designed to create a suitable path for the player.'
-    });
-  }).then(function (res) {
-    return botui.message.bot({
-      delay: 2400,
-      content: 'Or you can share third-party elements to clear your content and your conversations'
-    });
-  }).then(function (res) {
-    return botui.message.add({
-      delay: 2200,
-      type: 'embed',
-      content: 'https://giphy.com/embed/3ohze0jPWQJJ2EEo7K'
-    });
-  }).then(function (res) {
-    return botui.message.bot({
-      delay: 2500,
-      content: 'There is a lot more you can do with Bots check this !(book) [article](https://venturebeat.com/2017/07/20/the-future-of-enterprise-chatbots/)'
-    });
-  }).then(land);
+  })
+  .then(function (res) {
+    ga_record('btn_click', res.value); //google analitics lock execution
+    if(res.value == 'automotive1') {
+      automotive();
+    }
+    if(res.value == 'weather1') {
+      weather();
+    }
+    if(res.value == 'gps1') {
+      gps();
+    }
+    if(res.value == 'miscellaneous1') {
+      miscellaneous();
+    }
+  })
+  .then(askmore);
 };
 
-var land = function () {
-  //ga_record('message', 'end'); google analitics lock execution
+////////////////////////////////
+var aboutus = function () {
   botui.message.add({
-    delay: 2000,
-    content: 'Usually this kind of user experience is implemented in chatbots 🤖 for communication applications such as messenger, slack or snippet for web sites. But it can also be used differently:'
-  }).then(function () {
+    loading: true,
+    delay: 1000,
+    content: 'For more information about us visit our <b>[About Us Chatbot page](/about)</b>'
+  })
+  .then(askmore);
+};
+
+////////////////////////////////
+var contactus = function () {
+  botui.message.add({
+    loading: true,
+    delay: 1000,
+    content: 'To contact us visit our <b>[Contact Us Chatbot page](/contact)</b>'
+  })
+  .then(askmore);
+};
+
+////////////////////////////////
+var ourcustomers = function () {
+  botui.message.add({
+    loading: true,
+    delay: 1000,
+    content: 'To see  our recent customers visit our <b>[Recent Customers Chatbot page](/customers)</b>'
+  })
+  .then(askmore);
+};
+
+////////////////////////////////
+var exitnow = function () {
+  ga_record('message', 'end'); //google analitics lock execution
+  botui.message.add({
+    loading: true,
+    delay: 1000,
+    content: 'It is nice day to have some conversational with you <b>'+ guestname +'</b>'
+  })
+  .then(function () {
     return botui.message.bot({
       delay: 3000,
-      content: 'now you can also use conversational layouts in your landing page to promote your web bids!'
+      content: 'If you need further information drop me in line on !(mail) [contact@apexvalue.com](mailto:contact@apexvalue.com), I love meeting new people and making new friends!'
     });
-  }).then(function () {
-    return botui.message.add({
-      delay: 2200,
-      type: 'embed',
-      content: 'https://giphy.com/embed/129TvXmclvuuha'
-    });
-  }).then(function () {
-    return botui.message.bot({
-      delay: 2000,
-      content: 'By deburring !(book) [BotUI](https://github.com/moinism/botui), a new javascript framework, I have developed for you this example that is based on !(book) [Jekyll](https://jekyllrb.com/) hosted on !(github) [GitHub pages](https://pages.github.com/)'
-    });
-  }).then(function () {
-    return botui.message.bot({
-      delay: 4000,
-      content: 'Fork it out !(github) [here](https://github.com/conversationalui/conversationalui.github.io) and start build your conversional landing page!'
-    });
-  }).then(function () {
-    return botui.message.bot({
-      delay: 3000,
-      content: 'I hope you can come back to creating great communications!'
-    });
-  }).then(function () {
-    return botui.message.bot({
-      delay: 2000,
-      content: 'If you need further information or have any doubts, drop me in line on !(mail) [lantoniotrento@gmail.com](mailto:lantoniotrento@gmail.com), I am always happy to meet new people and create something extraordinary together!'
-    });
-  }).then(function () {
-    return botui.message.bot({
-      delay: 3000,
-      content: 'If you want to give me a hand to let you know this project in the world feel free to share !(twitter) [this tweet](https://twitter.com/lantoniotrento/status/894954672921997312) on my twitter account, I would be very grateful to you!'
-    });
-  }).then(function () {
+  })
+  .then(function () {
     return botui.message.bot({
       delay: 2000,
       content: '👋'
     });
   });
 };
+
+////////////////////////////////
+var askmore = function () {
+  botui.message.add({
+    loading: true,
+    delay: 1000,
+    content: 'What else do you need <b>'+guestname+'</b>?'
+  })
+  .then(function () {
+    return botui.action.button({
+      delay: 500,
+      action: [{
+        text: 'Get Started',
+        value: 'getstarted'
+      }, {
+        text: 'Other options',
+        value: 'moreoptions'
+      }]
+      });
+  })
+  .then(function (res) {
+    ga_record('btn_click', res.value); //google analitics lock execution
+    if(res.value == 'getstarted') {
+      getstarted();
+    }
+    if(res.value == 'moreoptions') {
+      moreoptions();
+    }
+    if(res.value == 'exitnow') {
+      exitnow();
+    }
+  });
+};
+
+////////////////////////////////
+var moreoptions = function () {
+  botui.message.add({
+    loading: true,
+    delay: 1000,
+    content: 'Kindly select the following options below:'
+  })
+  .then(function () {
+    return botui.action.button({
+      delay: 500,
+      size:30,
+      action: [{
+        text: 'About',
+        value: 'aboutus2'
+      }, {
+        text: 'Contact',
+        value: 'contactus'
+      }, {
+        text: 'Customers',
+        value: 'ourcustomers2'
+      }, {
+        text: 'End',
+        value: 'exitnow2'
+      }]
+      });
+  })
+  .then(function (res) {
+    ga_record('btn_click', res.value); //google analitics lock execution
+    if(res.value == 'aboutus2') {
+      aboutus();
+    }
+    if(res.value == 'contactus') {
+      contactus();
+    }
+    if(res.value == 'ourcustomers2') {
+      ourcustomers();
+    }
+    if(res.value == 'exitnow2') {
+      exitnow();
+    }
+  });
+};
+
 
 var ga_record = function(type, action) {
   if(ga) {
